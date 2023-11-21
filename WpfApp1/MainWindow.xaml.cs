443eaeca84a23adbc64cfa6c6f2caaba1a9695f0
User <User@WIN-QNF6889VOJQ>
2023-11-22 02:40:22 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Double a = Convert.ToDouble(textBoxA.Text);
+            Double b = Convert.ToDouble(textBoxB.Text);
+
+            Double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Double p = a + b + c;
+
+            TbAnswer.Text += c;
+            TbAnswer_Copy.Text += p;
+        }
+    }
+}
