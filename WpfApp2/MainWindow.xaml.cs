241ce916f55e1f9828425aa12219b253a01ad0ce
User <User@WIN-QNF6889VOJQ>
2023-11-22 15:19:45 +0300

    Все задания

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    
+    public partial class MainWindow : Window
+    {
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            Double x = Convert.ToDouble(textBoxY.Text);
+
+
+
+            Double y = 3 * Math.Pow(x, 6) - 6 * Math.Pow(x, 2) - 7; 
+
+            TbAnswer.Text += x;
+
+        }
+    }
+
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Double A = Convert.ToDouble(textBoxA.Text);
+
+            Double A2 = A * A;
+
+            Double A4 = A2 * A2;
+
+            Double A8 = A4 * A4;
+
+            TbAnswer.Text += A2;
+            TbAnswer_Copy.Text += A4;
+            TbAnswer_Copy1.Text += A8;
+        }
+    }
+}
